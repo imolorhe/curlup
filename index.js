cd7753356b13fab835383d374da2a23e7a25c61b
Samuel Imolorhe <samuelimolo4real@gmail.com>
2018-05-22 21:35:01 +0200

    Added initial commit.

---
+++ index.js
@@ -0,0 +1,163 @@
+
+const sum = (a, b) => {
+    return a + b;
+};
+
+const LITERAL_NAMES = {
+    CURL: 'curl',
+    INCLUDE_HEADERS: 'include-headers',
+    SILENT: 'run-silent',
+    USE_GET: 'use-get',
+    COMPRESSED: 'compressed',
+    STATES: {
+        HEADER: 'header-state',
+        CUSTOM_METHOD: 'custom-method-state',
+        DATA: 'data-state',
+        USER: 'user-state',
+    },
+    STRING: 'string',
+    OTHERS: 'others',
+};
+
+const literals = [
+    {
+        regex: /^\bcurl\b/,
+        name: LITERAL_NAMES.CURL
+    },
+    {
+        regex: /^(-H|--header)/,
+        name: LITERAL_NAMES.STATES.HEADER
+    },
+    {
+        regex: /^-X/,
+        name: LITERAL_NAMES.STATES.CUSTOM_METHOD
+    },
+    {
+        regex: /^(-u|--user)/,
+        name: LITERAL_NAMES.STATES.USER
+    },
+    {
+        regex: /^(-d|--data)/,
+        name: LITERAL_NAMES.STATES.DATA
+    },
+    {
+        regex: /^--compressed/,
+        name: LITERAL_NAMES.COMPRESSED
+    },
+    {
+        regex: /^(-i|--include)/,
+        name: LITERAL_NAMES.INCLUDE_HEADERS
+    },
+    {
+        regex: /^(-s|--silent)/,
+        name: LITERAL_NAMES.SILENT
+    },
+    {
+        regex: /^(-G|--get)/,
+        name: LITERAL_NAMES.USE_GET
+    },
+    {
+        regex: /^'[^']*'/,
+        name: LITERAL_NAMES.STRING
+    },
+    {
+        regex: /^"[^"]*"/,
+        name: LITERAL_NAMES.STRING
+    },
+    {
+        regex: /^[^\n\r\s]*/,
+        name: LITERAL_NAMES.STRING
+    },
+    {
+        regex: /^.*/,
+        name: LITERAL_NAMES.OTHERS
+    },
+];
+
+
+const trimValue = val => val.trim().replace(/^('|")\s*/, '').replace(/\s*('|")$/, '');
+const isURL = val => /((([A-Za-z]{3,9}:(?:\/\/)?)(?:[\-;:&=\+\$,\w]+@)?[A-Za-z0-9\.\-]+|(?:www\.|[\-;:&=\+\$,\w]+@)[A-Za-z0-9\.\-]+)((?:\/[\+~%\/\.\w\-_]*)?\??(?:[\-\+=&;%@\.\w_]*)#?(?:[\.\!\/\\\w]*))?)/.test(val);
+
+const parseCurl = curlCmd => {
+    let cmd = curlCmd;
+    const tokens = [];
+    const result = {
+        url: '',
+        method: 'GET',
+        headers: {},
+        auth: {}
+    };
+    while (cmd) {
+        literals.some(literal => {
+            if (literal.regex.test(cmd)) {
+                tokens.push({
+                    name: literal.name,
+                    value: cmd.match(literal.regex)
+                });
+    
+                cmd = cmd.replace(literal.regex, '').trim();
+                return true;
+            }
+        });
+    }
+
+    // console.log(JSON.stringify(tokens, null, 2));
+    if (tokens.length) {
+        if (tokens.shift().name !== 'curl') {
+            throw new Error('Not a curl command.');
+        }
+
+        let state = '';
+        tokens.forEach(token => {
+            if (!state) {
+                switch(token.name) {
+                    case LITERAL_NAMES.STATES.HEADER:
+                        state = LITERAL_NAMES.STATES.HEADER;
+                        break;
+                    case LITERAL_NAMES.STATES.DATA:
+                        state = LITERAL_NAMES.STATES.DATA;
+                        break;
+                    case LITERAL_NAMES.STATES.CUSTOM_METHOD:
+                        state = LITERAL_NAMES.STATES.CUSTOM_METHOD;
+                        break;
+                    case LITERAL_NAMES.STATES.USER:
+                        state = LITERAL_NAMES.STATES.USER;
+                        break;
+                    case LITERAL_NAMES.STRING: {
+                        const val = trimValue(token.value[0]);
+                        if (isURL(val)) {
+                            result.url = val;
+                        }
+                        break;
+                    }
+                }
+            } else {
+                switch(state) {
+                    case LITERAL_NAMES.STATES.HEADER: {
+                        const pair = trimValue(token.value[0]).split(/\s*:\s*/);
+                        result.headers[pair[0]] = pair[1];
+                        break;
+                    }
+                    case LITERAL_NAMES.STATES.DATA: {
+                        const pair = token.value[0];
+                        result.data = trimValue(token.value[0]).trim();
+                        break;
+                    }
+                    case LITERAL_NAMES.STATES.CUSTOM_METHOD: {
+                        const pair = token.value[0];
+                        result.method = trimValue(token.value[0]).trim();
+                        break;
+                    }
+                }
+                state = '';
+            }
+        });
+    }
+
+    return result;
+};
+
+module.exports = {
+    sum,
+    parseCurl
+};
---
+++ index.spec.js
@@ -0,0 +1,52 @@
+const jsCurl = require('./index');
+
+const pc = cmd => console.log(JSON.stringify(jsCurl.parseCurl(cmd), null, 2));
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(jsCurl.sum(1, 2)).toBe(3);
+});
+
+// https: //www.rosehosting.com/blog/curl-command-examples/
+test('parseCurl', () => {
+    const cmd = `curl 'http://google.com/' -H 'Accept-Encoding: gzip, deflate, sdch' -H 'Accept-Language: en-US,en;q=0.8,da;q=0.6' -H 'Upgrade-Insecure-Requests: 1' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.110 Safari/537.36' -H 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8' -H 'Connection: keep-alive' --compressed`;
+    pc(cmd);
+    // pc('curl https://domain.com');
+    // pc('curl -o website https://domain.com'); // *
+    // pc('curl -O https://domain.com/file.zip'); // *
+    // pc('curl -O https://domain.com/file.zip -O https://domain.com/file2.zip'); // *
+    // pc('curl -u user sftp://server.domain.com/path/to/file'); // *
+    // pc('curl -I http://domain.com'); // *
+    pc('curl ftp://ftp.domain.com --user username:password'); // *
+    pc('curl -T file.zip ftp://ftp.domain.com/ --user username:password'); // *
+    pc('curl -d password=x http://x.com/y'); // *
+    pc(`curl -i -H 'Accept: application/json' -X POST -F "filename=@/file/path" -F "other_field=its_value"   "http://hostname/resource"`);
+    pc(`curl -i -H 'Content-Type: application/json' -H 'Accept: application/json' -X DELETE -d '{"key1":"value1"}' "http://hostname/resource"`);
+    pc(`curl -s \
+     -X POST https://10.240.0.100:5000/v2.0/tokens \
+     -H "Content-Type: application/json" \
+     -d '{"auth": {"tenantName": "TENANT", "passwordCredentials":{"username": "USERNAME", "password": "PASSWORD"}}}'`);
+    pc(`curl -i \
+     -H "Content-Type: application/json" \
+     -d '
+        { "auth": {
+            "identity": {
+              "methods": ["password"],
+              "password": {
+                "user": {
+                  "name": "USERNAME",
+                  "domain": { "id": "default" },
+                  "password": "PASSWORD"
+                }
+              }
+            },
+            "scope": {
+              "project": {
+                "name": "PROJECT",
+                "domain": { "id": "default" }
+              }
+            }
+          }
+        }' \
+     https://10.240.0.100:5000/v3/auth/tokens`);
+    expect(3).toBe(3);
+});
